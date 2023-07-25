471d056494db88f46a0fbe55485d90f383ed0953
shyn le <highlandetspirou2812@gmail.com>
2023-07-25 10:50:38 +0700

    add basehead

--- src/components/Navbar/NavItems.js
+++
@@ -1,7 +0,0 @@
-const navItems = [
-  { id: "introduction", label: "Introduction" },
-  { id: "education", label: "Education" },
-  { id: "experiences", label: "Experiences" },
-];
-
-export default navItems
--- src/components/Navbar/NavState.jsx
+++
@@ -1,7 +0,0 @@
-import { createSignal, createRoot } from "solid-js";
-
-export const $navState = createRoot(function () {
-    const [currentSection, setCurrentSection] = createSignal("introduction");
-
-  return { currentSection, setCurrentSection };
-});
